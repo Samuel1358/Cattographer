37e48cd47b5696dbbd808fbb2ab1994920868dd4
Samuel1358 <bazonsamuel@gmail.com>
2024-03-24 18:32:20 -0300

    Sistema de sombras e recarga
    Scripts (Timer_Sombra e Reset_Sombra) quando o tempo das Sombras termina o jogo é resetado, assim como quando o tempo chega a 0, mas quando o Jogador entra em uma sala de Vazio ou de Saida o timer das Sombras reseta

---
+++ Assets/Timer_Sombra/Sombra/Timer_Sombra.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timer_Sombra : MonoBehaviour
+{
+    public float timerSombra = 15f, ttSombra;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        ttSombra = timerSombra;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timerSombra <= 0)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+        if (timerSombra > 0)
+        {
+            timerSombra -= Time.deltaTime;
+        }
+    }
+}
