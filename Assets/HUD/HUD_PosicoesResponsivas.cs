d879fbf013df2354793eb0ed45fad637f7a2b9b6
Samuel1358 <bazonsamuel@gmail.com>
2024-04-19 23:10:38 -0300

    Port pra mobile, parte 1-2
    Hud responsivo
     - os contadores de time, verde e amarelo, agora estão com as suas posições responsivas com o tamanho de tela

--- Assets/HUD/HUD_PosicoesResponsivas.cs
+++ Assets/HUD/HUD_PosicoesResponsivas.cs
@@ -8,6 +8,9 @@ public class HUD_PosicoesResponsivas : MonoBehaviour
 {
     public RectTransform canvas;
 
+    public RectTransform timer;
+    public RectTransform verdes;
+    public RectTransform amarelos;
     public RectTransform botoes_move;
 
     // Start is called before the first frame update
@@ -19,15 +22,45 @@ public class HUD_PosicoesResponsivas : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        #region // timer
+
+        /*botoes_move.anchorMin = new Vector2(0f, 1f);
+        botoes_move.anchorMax = new Vector2(0f, 1f);
+        botoes_move.pivot = new Vector2(0.5f, 0.5f);*/
+
+        timer.anchoredPosition = new Vector2(360 * (canvas.rect.width / 1080), -135 * (canvas.rect.height / 2460));
+
+        #endregion
+
+        #region // verde
+
+        /*botoes_move.anchorMin = new Vector2(1f, 1f);
+        botoes_move.anchorMax = new Vector2(1f, 1f);
+        botoes_move.pivot = new Vector2(0.5f, 0.5f);*/
+
+        verdes.anchoredPosition = new Vector2(-120 * (canvas.rect.width / 1080), -120 * (canvas.rect.height / 2460));
+
+        #endregion
+
+        #region // amarelo
+
+        /*botoes_move.anchorMin = new Vector2(1f, 1f);
+        botoes_move.anchorMax = new Vector2(1f, 1f);
+        botoes_move.pivot = new Vector2(0.5f, 0.5f);*/
+
+        amarelos.anchoredPosition = new Vector2(-120 * (canvas.rect.width / 1080), -210 * (canvas.rect.height / 2460));
+
+        #endregion
+
         #region // botoes_move
 
-        botoes_move.anchorMin = new Vector2(0.5f, 0.5f);
+        /*botoes_move.anchorMin = new Vector2(0.5f, 0.5f);
         botoes_move.anchorMax = new Vector2(0.5f, 0.5f);
-        botoes_move.pivot = new Vector2(0.5f, 0.5f);
-        Debug.Log(canvas.rect.height);
-        Debug.Log(canvas.rect.height / 2460);
-        botoes_move.anchoredPosition = new Vector2(0, -880 * (canvas.rect.height / 2460));
+        botoes_move.pivot = new Vector2(0.5f, 0.5f);*/
 
+        botoes_move.anchoredPosition = new Vector2(0, -880 * (canvas.rect.height / 2460));
+        
         #endregion
     }
 }
