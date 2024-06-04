6de7ba3178052f04e90f7deb486a3c8b90e322ed
Samuel1358 <bazonsamuel@gmail.com>
2024-06-04 01:59:15 -0300

    Preencher paredes
    Script 'PreencherModelos' adiciona os modelos das paredes de acordo com seu tamanho, agrupa na hierarquia e torna todos static

--- Assets/Obstaculos/Scripts/PreencherModelos.cs
+++ Assets/Obstaculos/Scripts/PreencherModelos.cs
@@ -1,20 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class PreencherModelos : MonoBehaviour
 {
-    //Get
+    public GameObject modelo;
+    public float altura;
 
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+        int x = (int)Mathf.Ceil(Mathf.Ceil(transform.localScale.x - modelo.transform.localScale.x) / modelo.transform.localScale.x),
+            z = (int)Mathf.Ceil(Mathf.Ceil(transform.localScale.z - modelo.transform.localScale.z) / modelo.transform.localScale.z);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+        float a, b;
+
+        /*if (altura == null)
+        {
+            altura = -(transform.localScale.y / 2f);
+        }*/
+
+        GameObject instance;
+
+        GameObject cubo = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cubo.transform.position = new Vector3(0, -10, 0);
+        cubo.name = "Modelos";
+
+        for (float i = -(x/2f); i <= x/2f; i += modelo.transform.localScale.x)
+        {
+            for (float k = -(z / 2f); k <= z / 2f; k += modelo.transform.localScale.z)
+            {
+                if (Mathf.Abs(transform.rotation.eulerAngles.y / 90) % 2 == 0)
+                {
+                    a = i;
+                    b = k;
+                }
+                else
+                {
+                    a = k;
+                    b = i;
+                }
+                Vector3 pos = new Vector3(transform.position.x + a, transform.position.y + /*(float)*/altura, transform.position.z + b);
+                instance = Instantiate(modelo, pos, Quaternion.identity, cubo.transform);
+                instance.isStatic = true;
+            }
+        }
+
+        cubo.isStatic = true;
+        cubo.transform.SetParent(this.transform);
     }
 }
