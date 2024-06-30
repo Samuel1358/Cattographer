4d6b1fa1c1346e8b70a9beaf22d31d87601f3950
Samuel1358 <bazonsamuel@gmail.com>
2024-06-30 16:53:34 -0300

    indicação da saida

--- Assets/Gerador_Sala/Prefebs/Saida/LuzDeSaida.cs
+++ Assets/Gerador_Sala/Prefebs/Saida/LuzDeSaida.cs
@@ -4,7 +4,25 @@ using UnityEngine;
 
 public class LuzDeSaida : MonoBehaviour
 {
-    [SerializeField] GameObject luz;
+    [SerializeField] GameObject luz;// braseiro1, braseiro2;
+    [SerializeField] GameObject braseiro;
+    Braseiro bras1, bras2;
+
+
+    private void Start()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, 1.5f))
+        {
+            if (hit.collider.gameObject.CompareTag("Parede"))
+            {
+                //braseiro1.SetActive(true);
+                //braseiro2.SetActive(true);
+                bras1 = Instantiate(braseiro, transform.position - transform.forward - (transform.right * 2), Quaternion.identity, this.transform).GetComponent<Braseiro>();
+                bras2 = Instantiate(braseiro, transform.position - transform.forward + (transform.right * 2), Quaternion.identity, this.transform).GetComponent<Braseiro>();
+            }
+        }
+    }
 
     public Vector3 Verificar()
     {
@@ -14,6 +32,10 @@ public class LuzDeSaida : MonoBehaviour
         {
             if (hit.collider.gameObject.CompareTag("Parede"))
             {
+                //braseiro1.GetComponent<Braseiro>().Acender();
+                //braseiro2.GetComponent<Braseiro>().Acender();
+                bras1.Acender();
+                bras2.Acender();
                 luz.SetActive(true);
                 r = transform.position;
             }
